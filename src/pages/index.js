0aa837e6a49f16bfe892de2b3e9ac9639f841959
Thom Bat <thomas.batard@gmail.com>
2020-08-26 22:48:26 +0200

    test ajout liens sur home page avec style flex dans css global

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,12 @@ import nazila_welcome from "./nazila_welcome.jpg"
 export default function Home() {
   return (
     <div>
+      <div className="liens">
       <Link to="/contact/">Contact</Link>
+      <Link to="/about/">About</Link>
+      <Link to="/about-css-modules/">About CSS modules</Link>
+      </div>
+      
       <Header headerText="Welcome to my world !" />
       <p>Hope you like it.</p>
     <img src={nazila_welcome} alt="" />

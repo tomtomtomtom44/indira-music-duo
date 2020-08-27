2ba35fe5aa9884ec0cccf9c2b62c21cb3bade2ac
Thomas Batard <thomas.batard@gmail.com>
2020-08-27 18:44:49 +0200

    test ajout du plugin typography

---
+++ src/utils/typography.js
@@ -0,0 +1,6 @@
+import Typography from "typography"
+import fairyGateTheme from "typography-theme-funston"
+
+const typography = new Typography(fairyGateTheme)
+export const { scale, rhythm, options } = typography
+export default typography
